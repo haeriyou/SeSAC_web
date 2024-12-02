c821cada5955c6f81362b9634dcc31f9ad51d303
haeriyou <haeri1you@gmail.com>
2024-12-02 11:23:59 +0900

    241202 MVC 실습1 수정

--- 13_MVC_Practice/app.js
+++ 13_MVC_Practice/app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// 미들웨어 설정
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+// body-parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Router
 const routerIndex = require("./routes/Rindex");
+// const routerIndex = require("./routes"); index가 default이므로 routes까지만 적어도 된다.
 app.use("/", routerIndex);
 
 // 404
--- 13_MVC_Practice/controller/Cmain.js
+++ 13_MVC_Practice/controller/Cmain.js
@@ -15,3 +15,22 @@ exports.userCheck = (req, res) => {
     res.send({ isSuccess: false });
   }
 };
+
+// POST '/login'
+/*exports.userCheck = (req, res) => {
+  // console.log(req.body);
+  // { userId: 'ddd', userPw: 'ddd' }
+  // console.log("model>>> ", User.getUserInfo());
+  // { realId: 'banana', realPw: '1234' }
+
+  // 객체 구조 분해 추가
+  const { realId, realPw } = User.getUserInfo(); //{ realId: 'banana', realPw: '1234' }
+  // console.log("-----");
+  // console.log(realId, realPw);
+  const { userId, userPw } = req.body;
+  if (realId === userId && realPw === userPw) {
+    res.send({ userInfo: req.body, isSuccess: true });
+  } else {
+    res.send({ isSuccess: false });
+  }
+}; */
